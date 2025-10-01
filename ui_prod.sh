d1362ebcad8e8dadf6f2e42dee733918fd581212
Tyrone Nicholas <tyrone.nicholas@gmail.com>
2025-10-01 11:45:03 +0100

    Finally finished week 1

--- ui_prod.sh
+++ ui_prod.sh
@@ -3,11 +3,12 @@
 # To be run on UI server
 
 docker rm -f ui
+docker pull docker.io/tyronen24/ui:latest
+
 docker run \
     -d \
     --name ui \
     -p 80:80 \
     --env-file ~/.env.prod \
     --restart unless-stopped \
-    --pull=always \
     docker.io/tyronen24/ui:latest
